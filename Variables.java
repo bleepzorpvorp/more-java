b0a526eb91a51399ccb040ac5a8a0852c20ab26d
da best the one and only <makcooper22@gmail.com>
2025-12-04 14:18:04 +0000

    commit

---
+++ Variables.java
@@ -0,0 +1,27 @@
+// Two types of variables: instance variables and static variables
+// variables use camelCase
+// constants use uppercase
+class Student {
+    String studentName; // instance variable, belongs  to object
+    static int studentCount = 0; // static variable, belongs to class
+    static final String SCHOOL_NAME = "CSAEA"; // snake_case with final
+
+    Student(String n) {
+        studentName = n; //belong to specific object
+        System.out.println(n + " is a student at " + SCHOOL_NAME);
+        studentCount++;
+        System.out.println("Number of students: " + studentCount);
+    }
+}
+public class Variables {
+    public static  void main(String[] args) {
+        new Student("Alex");
+        new Student("Joe");
+        new Student("Tian");
+        new Student("Chase");
+        new Student("Gabbo");
+        new Student("Tai");
+        new Student("Arin");
+ 
+    }
+}
