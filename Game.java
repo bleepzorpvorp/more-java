d50ef15f04b97408293da236b59efbaa74543807
da best the one and only <makcooper22@gmail.com>
2025-12-19 13:31:27 +0000

    me do later

---
+++ Game.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Game {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = input.nextLine();
+    int playerHealth = 100;
+    int playerPoints = 0;
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("A. ");
+    System.out.println("B. ");
+    String decision1 = input.nextLine();
+    if (decision1.equals("A")) {
+        System.out.println("");
+    }
+    else if (decision1.equals("B")) {
+        System.out.println("");
+        System.out.println("");
+        
+    }
+        //first  decision
+        //first decision leads to continuation or a fight with former president obama
+        //if continued 
+
+
+
+    }
+}
