bfdd7b053c38da334b3e35f4838ca7f052782fcf
da best the one and only <makcooper22@gmail.com>
2026-01-19 15:47:57 +0000

    array 2

--- fundamentals/Arrays/TicTacToe.java
+++ fundamentals/Arrays/TicTacToe.java
@@ -6,6 +6,7 @@ public class TicTacToe {
         int columns = 3;
         char[][] board = new char[rows][columns];
         Random rand = new Random();
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < columns; c++) {
                 if (rand.nextInt(2) == 0) {
@@ -15,11 +16,40 @@ public class TicTacToe {
                 }
             }
         }
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < columns; c++) {
                 System.out.print(board[r][c] + " ");
             }
-            System.out.println(); 
+            System.out.println();
         }
+
+        char winner = checkWinner(board);
+
+        if (winner == 'X') {
+            System.out.println("X wins!");
+        } else if (winner == 'O') {
+            System.out.println("O wins!");
+        } else {
+            System.out.println("Draw");
+        }
+    }
+
+    public static char checkWinner(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return board[i][0];
+            }
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return board[0][i];
+            }
+        }
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return board[0][0];
+        }
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return board[0][2];
+        }
+        return ' ';
     }
 }
